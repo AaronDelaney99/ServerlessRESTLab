e9da717114eb84e65f4252acd4e8dc1ea5084f0b
Sadge-Student <20092443@mail.wit.ie>
2023-11-19 00:55:04 +0000

    Add new movie endpoint.

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -54,6 +54,20 @@ export class RestAPIStack extends cdk.Stack {
           },
         }
         );
+
+        const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+          architecture: lambda.Architecture.ARM_64,
+          runtime: lambda.Runtime.NODEJS_16_X,
+          entry: `${__dirname}/../lambdas/addMovie.ts`,
+          timeout: cdk.Duration.seconds(10),
+          memorySize: 128,
+          environment: {
+            TABLE_NAME: moviesTable.tableName,
+            REGION: "eu-west-1",
+          },
+        });
+        
+        moviesTable.grantReadWriteData(newMovieFn)
         
         new custom.AwsCustomResource(this, "moviesddbInitData", {
           onCreate: {
@@ -93,6 +107,10 @@ export class RestAPIStack extends cdk.Stack {
         moviesEndpoint.addMethod(
           "GET",
           new apig.LambdaIntegration(getAllMoviesFn, { proxy: true })
+        );
+        moviesEndpoint.addMethod(
+          "POST",
+          new apig.LambdaIntegration(newMovieFn, { proxy: true })
         );
     
         const movieEndpoint = moviesEndpoint.addResource("{movieId}");
